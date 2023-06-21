7031eb15ef530feb042900f82e8a560a86e315b3
mtaharrouchte <mohamedtaharrouchte@gmail.com>
2023-06-20 18:36:55 -0700

    adjust source code to Betty style

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,19 @@
 #include "main.h"
 #define MAX_CHAR 9
 
-int main(void){
+/**
+ * main - program entry point
+ *
+ * Return: On success 0
+ */
+int main(void)
+{
 
 	char charCount = 0u;
-	char* string_ptr = "_putchar\n";
+	char *string_ptr = "_putchar\n";
 
-	while(charCount++ < MAX_CHAR){
+	while (charCount++ < MAX_CHAR)
+	{
 		_putchar(*(string_ptr + charCount - 1));
 	}
 
