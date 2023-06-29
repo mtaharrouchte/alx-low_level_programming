93c64c149a04d9f67f780c0ab601c8459bee7d12
mtaharrouchte <mohamedtaharrouchte@gmail.com>
2023-06-29 10:11:10 -0700

    perform swap function

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* swap_int - swap two numbers integer
+* @a - pointer to integer a
+* @b - pointer to integer b
+* Return: Always 0.
+*/
+void swap_int(int *a, int *b)
+{
+	int temp = 0;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+
+}
