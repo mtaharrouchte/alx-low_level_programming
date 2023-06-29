75f6005d0e96e9d0b8760ced9ea4f2956ed2effe
mtaharrouchte <mohamedtaharrouchte@gmail.com>
2023-06-29 16:59:28 -0700

    update #2

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -13,7 +13,6 @@ void print_rev(char *s)
 	const char *null_ptr_err = "NULL PTR EXCEPTION";
 	const int s_size = strlen(s);
 	char *rev_s = (char *) malloc(s_size);
-	
 	if (s != NULL && rev_s != NULL)
 	{
 		int index = 0;
