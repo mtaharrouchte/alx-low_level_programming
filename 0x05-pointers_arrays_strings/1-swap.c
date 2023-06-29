6432966cf17ba3d259165eebbae2927f95243dbe
mtaharrouchte <mohamedtaharrouchte@gmail.com>
2023-06-29 10:16:08 -0700

    correct documentation syntaxe betty style

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,8 +3,8 @@
 
 /**
 * swap_int - swap two numbers integer
-* @a - pointer to integer a
-* @b - pointer to integer b
+* @a:  pointer to integer a
+* @b:  pointer to integer b
 * Return: Always 0.
 */
 void swap_int(int *a, int *b)
