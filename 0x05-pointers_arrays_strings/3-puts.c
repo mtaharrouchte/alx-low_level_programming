4f465ebd14ebaaf2efa91b9f8696793b21b551e2
mtaharrouchte <mohamedtaharrouchte@gmail.com>
2023-06-29 12:30:56 -0700

    write to a stream

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+* _puts - puts wrapper
+* @str: pointer to string to write to a stream
+* Return: Always 0.
+*/
+void _puts(char *str)
+{
+
+	puts(str);
+
+}
